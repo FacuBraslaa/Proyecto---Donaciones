2bc68b2bf3d3930b86f2843f421f95328828bcc2
BRASLAVSKY FACUNDO NICOLAS <48245759@ort>
2024-08-06 11:11:54 -0300

    elimine cosa inservibles

--- Union.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysq12/promise";
-
-export const conn = await mysql.createConection({
-    host:"localhost",
-    user: "root",
-    password: "rootroot",
-    datbase: "base de datos de aplicacion de donaciones",
-})
-
