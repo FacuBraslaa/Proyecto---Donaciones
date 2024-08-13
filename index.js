29839ff94d00e2ae0346e2c580e2ddfac7312bd9
BRASLAVSKY FACUNDO NICOLAS <48245759@ort>
2024-08-13 13:27:24 -0300

    Se termiino de fix la comprobacion, ahora se pude comenzar a programar

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-import Donantes from "./Donantes.js";
-import ongosc from "./ongosc.js";
-import Opciones from "./Opciones.js";
-import Like from "./Like.js";
+
 
 app.use(express.json());
 
-app.get("/", (_, res) => {
+app.get("/chau", (_, res) => {
     res.send("Proyecto funcionando");
 });
 
