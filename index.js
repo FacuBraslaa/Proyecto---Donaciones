a943d1495df84d8086964b0ec8c89889a9c60cbb
BRASLAVSKY FACUNDO NICOLAS <48245759@ort>
2024-08-22 12:06:34 -0300

    agregar mucho pero no funciona

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-import express from "express";
+const express = require ("express")
 const app = express();
 const port = 3000;
-
-
 
 app.use(express.json());
 
@@ -13,3 +11,11 @@ app.get("/chau", (_, res) => {
 app.listen(3000, ()=>{
     console.log("Mano a Mano app listening in port 3000")
 })
+
+//conexion
+const userrouter = require ("./routes/users")
+app.use("/users", usersrouter)
+
+
+
+app.get('/Donantes', donantes.getdonantes)
