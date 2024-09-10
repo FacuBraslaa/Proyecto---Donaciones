27d9ef04f8d27128043b38f0ee3e189c6ae4f400
BRASLAVSKY FACUNDO NICOLAS <48245759@ort>
2024-09-10 13:38:45 -0300

    Update Opciones.js

--- Controllers/Opciones.js
+++ Controllers/Opciones.js
@@ -4,20 +4,25 @@ import { client } from '../dbconfig.js'; // Asegúrate de que 'client' se export
 const crearOpciones = async (req, res) => {
     const { Nombres } = req.body;
 
+    // Consulta SQL con RETURNING para obtener el IdOpciones generado
     const query = `
         INSERT INTO "Opciones" 
             ("Nombres")
             VALUES ($1)
+            RETURNING "idOpciones"
     `;
 
     try {
-        await client.query(query, [Nombres]);
-        res.json({ message: "Opción registrada correctamente" });
+        const result = await client.query(query, [Nombres]);
+        const idOpciones = result.rows[0].IdOpciones; // Extraer el IdOpciones del resultado
+        res.json({ message: "Opción registrada correctamente", idOpciones });
     } catch (error) {
         console.error('Error al registrar Opción:', error);
         res.status(500).json({ message: "Error al registrar Opción", error: error.message });
     }
 };
+
+
 
 
 // Exportamos las funciones que manejan las Opciones
