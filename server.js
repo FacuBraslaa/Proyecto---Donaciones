ed5300d5ef6dd0c33760cc29cb47ed2b06c1d1c6
BRASLAVSKY FACUNDO NICOLAS <48245759@ort>
2024-11-26 11:57:52 -0300

    cors

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
 
 // Configuración básica de CORS
-app.use(cors()); 
+app.use(cors({
+    origin: '*', // Origen permitido
+    methods: ['GET', 'POST','PUT', 'OPTIONS'], // Métodos permitidos
+     allowedHeaders : ['Content-Type','Authorization'], // Cabeceras permitidas
+     credentials : true // Permitir credenciales
+})) 
 
 // Middleware para procesar JSON
 app.use(express.json());
 
-// Endpoint ejemplo
+// Endpoint para manejar solicitudes POST en /donantes
 app.post('/donantes', (req, res) => {
-    console.log(req.body); // Datos enviados desde el frontend
-    res.status(200).json({ message: 'Cuenta creada correctamente' });
+    console.log('Datos recibidos:', req.body); // Muestra los datos enviados desde el frontend
+    res.status(200).json({ message: 'Cuenta creada correctamente' }); // Respuesta de éxito
 });
 
 // Iniciar el servidor
@@ -19,3 +25,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
