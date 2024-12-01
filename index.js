12d91d5ce0829f13d8a67defab07d740363a0af1
FacuBraslaa <facundobraslavsky@gmail.com>
2024-12-01 20:45:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ import DonacionesRouter from './Routes/DonacionesRouter.js';  // Corregido: camb
 
 const app = express();
 const port = 3000;
+
 app.use((req, res, next) => {
+    console.log('HOLAAAAA')
     res.header('Access-Control-Allow-Origin', '*'); // Origen permitido
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Métodos permitidos
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Encabezados permitidos
